e3af1979e1543eb0ab06bc8a409c13eba5a32afe
vmishchenko <vladislav.a.mishchenko@gmail.com>
2020-01-07 17:43:50 +0200

    Updates

---
+++ precache-manifest.ffd4bdc704b5877a9ad68b2ec5d451fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d52979726751c617cdd8775c8479d7af",
+    "url": "/react_airport/index.html"
+  },
+  {
+    "revision": "acaa7a5125a8aaf72b79",
+    "url": "/react_airport/static/css/main.28106b39.chunk.css"
+  },
+  {
+    "revision": "ef1147a4395774df0d93",
+    "url": "/react_airport/static/js/2.2cd16468.chunk.js"
+  },
+  {
+    "revision": "acaa7a5125a8aaf72b79",
+    "url": "/react_airport/static/js/main.d9d155b0.chunk.js"
+  },
+  {
+    "revision": "c8cd8e70b1c2d1cd9765",
+    "url": "/react_airport/static/js/runtime~main.037fa8df.js"
+  }
+]);
